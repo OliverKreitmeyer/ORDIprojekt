6c893c551c961ab7adaa0c3def3c33e42c7e6a2d
dean-rakic <dr55241@fer.hr>
2025-01-30 16:12:42 +0100

    level 1 flip try

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -8,32 +10,44 @@ public class PlayerMovement : MonoBehaviour
     private float speed = 8f;
     private float jumpingPower = 16f;
     private bool isFacingRight = true;
+    private bool isOnTheFloor = false;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
 
     // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
+        FlipController();
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && isOnTheFloor)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            isOnTheFloor = false;
+            animator.SetBool("isJumping", !isOnTheFloor);
         }
 
         if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
-        Flip();
+        
     }
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector2 (horizontal * speed, rb.velocity.y);
-
+        animator.SetFloat("xVelo", Math.Abs(rb.velocity.x));
+        animator.SetFloat("yVelo", rb.velocity.y);
     }
 
     private bool isGrounded()
@@ -41,15 +55,30 @@ public class PlayerMovement : MonoBehaviour
         return Physics2D.OverlapCircle(groundCheck.position,0.2f,groundLayer);
     }
 
+    private void FlipController()
+    {
+        if (rb.velocity.x < 0 && isFacingRight)
+        {
+            Flip();
+        }
+        else if (rb.velocity.x > 0 && !isFacingRight)
+        {
+            Flip();
+        }
+
+    }
+
     private void Flip()
     {
-        if (isFacingRight && horizontal < 0f || isFacingRight && horizontal > 0f)
-        {
-            isFacingRight = !isFacingRight;
-            Vector3 localScale = transform.localScale;
-            localScale.x *= -1f;
-            transform.localScale = localScale;  
-        }
+        isFacingRight = !isFacingRight;
+        animator.transform.Rotate(0,180,0);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isOnTheFloor = true;
+        animator.SetBool("isJumping", !isOnTheFloor);
     }
 }
 
